e0d2ac7e4d8b43fa0696acc5e3beb46d3c429ef6
paletGerardo <paletgerardo@gmail.com>
2018-04-22 16:08:03 -0300

    lista html sin css armada

--- index.php
+++ index.php
@@ -1,12 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
+
 <body>
+  <h1>programacion web 2
+    <span>trabajos practicos:</span>
+  </h1>
+  <h2>grupo 1</h2>
+
+  <ul>
+    <li>
+      <a href="ejercicio1">Ejercicio Nro. 1: Escribir un programa php que dada una variable dada, imprima en el navegador del cliente, su tipo
+        de dato y su contenido. Formatee mediante el uso de HTML</a>
+    </li>
+    <li>
+      <a href="ejercicio2">Ejercicio Nro. 2: Escribir un programa php que calcule el área de un triángulo.</a>
+    </li>
+    <li>
+      <a href="ejercicio3">Ejercicio Nro. 3: Escribir un programa php que calcule el área de un rectángulo</a>
+    </li>
+    <li>
+      <a href="ejercicio4">Ejercicio Nro. 4: Escribir un programa php que calcule el área de un trapecio.</a>
+    </li>
+    <li>
+      <a href="ejercicio5">Ejercicio Nro. 5: Escribir un programa php que intercambie el contenido de dos variables.
+      </a>
+    </li>
+  </ul>
+  <h2>grupo 2</h2>
+
+  <ul>
+    <li>
+      <a href="ejercicio1">Ejercicio Nro. 1: Escribir un programa php que dada una variable dada, imprima en el navegador del cliente, su tipo
+        de dato y su contenido. Formatee mediante el uso de HTML</a>
+    </li>
+    <li>
+      <a href="ejercicio2">Ejercicio Nro. 2: Escribir un programa php que calcule el área de un triángulo.</a>
+    </li>
+    <li>
+      <a href="ejercicio3">Ejercicio Nro. 3: Escribir un programa php que calcule el área de un rectángulo</a>
+    </li>
+    <li>
+      <a href="ejercicio4">Ejercicio Nro. 4: Escribir un programa php que calcule el área de un trapecio.</a>
+    </li>
+    <li>
+      <a href="ejercicio5">Ejercicio Nro. 5: Escribir un programa php que intercambie el contenido de dos variables.
+      </a>
+    </li>
+  </ul>
   
+  <h2>grupo 3</h2>
+
+  <ul>
+    <li>
+      <a href="ejercicio1">Ejercicio Nro. 1: Escribir un programa php que dada una variable dada, imprima en el navegador del cliente, su tipo
+        de dato y su contenido. Formatee mediante el uso de HTML</a>
+    </li>
+    <li>
+      <a href="ejercicio2">Ejercicio Nro. 2: Escribir un programa php que calcule el área de un triángulo.</a>
+    </li>
+    <li>
+      <a href="ejercicio3">Ejercicio Nro. 3: Escribir un programa php que calcule el área de un rectángulo</a>
+    </li>
+    <li>
+      <a href="ejercicio4">Ejercicio Nro. 4: Escribir un programa php que calcule el área de un trapecio.</a>
+    </li>
+    <li>
+      <a href="ejercicio5">Ejercicio Nro. 5: Escribir un programa php que intercambie el contenido de dos variables.
+      </a>
+    </li>
+  </ul>
+
+  <section id="ejercicio1">
+    b4
+  </section>
 </body>
+
 </html>
